9dcfdbe3c51d22bb814d6f4370945bf152a5080c
stiven <sduque866@gmail.com>
2018-03-19 21:37:56 -0500

    se agrega el SplashScreen a la actividad v2

--- app/src/main/java/com/stivenduque/clinicapp/SplashScreen.java
+++ app/src/main/java/com/stivenduque/clinicapp/SplashScreen.java
@@ -1,24 +1,35 @@
 package com.stivenduque.clinicapp;
 
 import android.content.Intent;
-import android.os.Handler;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
+
+        TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, Login.class);
+                Intent intent = new Intent().setClass(SplashScreen.this, Login.class);
                 startActivity(intent);
                 finish();
             }
-        }, 4000);
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task,4000);
 
     }
 }

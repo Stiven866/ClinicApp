1173524dea4ee3ec1edfd9dc37664bd79e74f350
stiven-duque <sduque866@gmail.com>
2018-05-01 13:04:24 -0500

    Logueos

---
+++ app/src/main/java/com/stivenduque/clinicapp/entidades/User.java
@@ -0,0 +1,31 @@
+package com.stivenduque.clinicapp.entidades;
+
+public class User {
+    private String id;
+    private String name;
+    private String lasName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLasName() {
+        return lasName;
+    }
+
+    public void setLasName(String lasName) {
+        this.lasName = lasName;
+    }
+}

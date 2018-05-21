91f57c3889ffb8cc13090e4a0eb0298ba75fe1fd
stiven-duque <sduque866@gmail.com>
2018-05-21 02:15:51 -0500

    Se crea el paquete fragment y se separan los fragmentos

---
+++ app/src/main/java/com/stivenduque/clinicapp/Adapter/UserAdapter.java
@@ -0,0 +1,56 @@
+package com.stivenduque.clinicapp.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.stivenduque.clinicapp.Entidades.User;
+import com.stivenduque.clinicapp.R;
+
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder>{
+
+    List<User> userList;
+    public UserAdapter(List<User> userList){
+        this.userList = userList;
+    }
+
+
+    @Override
+    public UserHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_list,parent,false);
+        RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(layoutParams);
+        return new UserHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(UserHolder holder, int position) {
+
+        holder.tvName.setText(userList.get(position).getPhone());
+        holder.tvPhone.setText(userList.get(position).getPhone());
+        holder.tvDocument.setText(userList.get(position).getId());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class UserHolder extends RecyclerView.ViewHolder{
+
+        TextView tvDocument, tvName,tvPhone;
+        public UserHolder(View itemView) {
+            super(itemView);
+
+            tvDocument = itemView.findViewById(R.id.tv_document_my_medics);
+            tvName = itemView.findViewById(R.id.tv_name_my_medics);
+            tvPhone = itemView.findViewById(R.id.tv_phone_my_medics);
+        }
+    }
+}
--- app/src/main/java/com/stivenduque/clinicapp/Fragments/MyMedicsFragment.java
+++ app/src/main/java/com/stivenduque/clinicapp/Fragments/MyMedicsFragment.java
@@ -1,19 +1,103 @@
 package com.stivenduque.clinicapp.Fragments;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.stivenduque.clinicapp.Adapter.UserAdapter;
+import com.stivenduque.clinicapp.Entidades.User;
 import com.stivenduque.clinicapp.R;
 
-public class MyMedicsFragment extends Fragment {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MyMedicsFragment extends Fragment implements  com.android.volley.Response.Listener<JSONObject>, com.android.volley.Response.ErrorListener {
+
+    RecyclerView recyclerViewUser;
+    ArrayList<User> userArrayList;
+
+    ProgressDialog progressDialog;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+    private String idPreferences;
+    SharedPreferences sharedPreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.my_medics_fragment, container, false);
+        View view = inflater.inflate(R.layout.my_medics_fragment, container, false);
+        userArrayList = new ArrayList<>();
+        recyclerViewUser = view.findViewById(R.id.rv_my_medics);
+        recyclerViewUser.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerViewUser.setHasFixedSize(true);
+
+        request = Volley.newRequestQueue(getContext());
+
+        loadWebService();
+        //return inflater.inflate(R.layout.my_medics_fragment, container, false);
+        return view;
+    }
+
+    private void loadWebService() {
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Consultando...");
+        progressDialog.show();
+        loadPreferences();
+        String url =  getResources().getString(R.string.url)+ "Consulta_medics.php?user_id="+idPreferences;
+        Log.d("UURRLL",url);
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null, this, this);
+        request.add(jsonObjectRequest);;
+     }
+    private void loadPreferences(){
+        sharedPreferences = this.getActivity().getSharedPreferences("Credenciales", Context.MODE_PRIVATE);
+        idPreferences = sharedPreferences.getString("id","0");
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getContext(), "No hay consulta "+ error.toString(),Toast.LENGTH_SHORT).show();
+        Log.d("NoExiste",error.toString());
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        User user = null;
+        JSONArray jsonArray = response.optJSONArray("Medics");
+        try {
+            for (int i = 0; i<jsonArray.length();i++){
+                user = new  User();
+                JSONObject jsonObject = null;
+                jsonObject = jsonArray.getJSONObject(i);
+                user.setId(jsonObject.optString("id"));
+                user.setName(jsonObject.optString("name"));
+                user.setPhone(jsonObject.optString("phone"));
+                userArrayList.add(user);
+                }
+                progressDialog.hide();
+            UserAdapter userAdapter  = new UserAdapter(userArrayList);
+            recyclerViewUser.setAdapter(userAdapter);
+            } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d("soservices", "sos" + e.getMessage());
+        }
+
     }
 }
